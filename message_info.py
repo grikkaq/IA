fd58e78ecefe8e00a2d370eb6e42fef466b1fbd6
grikkaq <melnikov@campus.technion.ac.il>
2019-08-14 21:22:19 +0300

    Update message_info.py

--- message_info.py
+++ message_info.py
@@ -1,16 +1,20 @@
 #   Assuming EOL control character is LF
 import re
+#import io
 
 f = fopen(sys.args[1], "rb")
 f_splitted = re.split("\n\n", f) # headers + data
 
-# first line either METHOD + ARG + HTTP_Ver, or HTTP_Ver + STATUS + REASON_PHRASE
-ffline = f_splitted[0].readline().split(" ", 2)
+#   first line either METHOD + ARG + HTTP_Ver, or HTTP_Ver + STATUS + REASON_PHRASE
+ffline = f_splitted[0].split("\n", 1)[0].split(" ", 2)
+ffline = list(filter(None, ffline)) # drop empty strings
+
 if ( re.match( r"HTTP+", ffline[0], re.I ) ):
     out1 = ffline[1] + " " + ffline[2]
 else:
-    x = re.search("Host:[^\\n]*\\n", f_splitted[0] + "\n") # +"\n" in case and Host is the last header 
-    host =  f_splitted[0][ x.start() + 5 : x.end() - 1] if x else ""  
+    x = re.search("Host: [^\\n]+", f_splitted[0] + "\n") # +"\n" in case and Host is the last header 
+    host =  f_splitted[0][ x.start() + len("Host: ") : x.end() ] if x else ""  # port is included
     abs_path = ffline[1].split("?", 1)[0] if len(ffline) == 3 else "/"
+    out1 = "http://" + host + abs_path
     
-print(  )
+print( out1, end = " " )
