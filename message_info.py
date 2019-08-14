df95171fb7cb255f1eecbc836286c0652abfa7cf
grikkaq <melnikov@campus.technion.ac.il>
2019-08-14 19:41:30 +0300

    Update message_info.py

--- message_info.py
+++ message_info.py
@@ -6,7 +6,7 @@ f_splitted = re.split("\n\n", f) # headers + data
 
 # first line either METHOD + ARG + HTTP_Ver, or HTTP_Ver + STATUS + REASON_PHRASE
 ffline = f_splitted[0].readline().split(" ", 2)
-if ( re.match( r"HTTP+", ffline[0], re.l ) ):
+if ( re.match( r"HTTP+", ffline[0], re.I ) ):
     out1 = ffline[1] + " " + ffline[2]
 else:
     x = re.search("Host:[^\\n]*\\n", f_splitted[0] + "\n") # +"\n" in case and Host is the last header 
