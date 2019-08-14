72788e64d3595f19eebee8bc4757ca080186c62a
grikkaq <melnikov@campus.technion.ac.il>
2019-08-14 19:27:53 +0300

    Update message_info.py

--- message_info.py
+++ message_info.py
@@ -6,10 +6,11 @@ f_splitted = re.split("\n\n", f) # headers + data
 
 # first line either METHOD + ARG + HTTP_Ver, or HTTP_Ver + STATUS + REASON_PHRASE
 ffline = f_splitted[0].readline().split(" ", 2)
-if ( ffline[0] == "HTTP1" or ffline[0] == "HTTP/1.1"):
+if ( re.match( r"HTTP+", ffline[0], re.l ) ):
     out1 = ffline[1] + " " + ffline[2]
 else:
-    x = re.search("Host:[^\\n]\\n", f_splitted[0] + "\n") # +"\n" in case and Host is the last header 
-    url =  f_splitted[0][ x.start() + 5 : x.end() - 1] if x else ""
-    url = url + ffline[1] # assuming here
+    x = re.search("Host:[^\\n]*\\n", f_splitted[0] + "\n") # +"\n" in case and Host is the last header 
+    host =  f_splitted[0][ x.start() + 5 : x.end() - 1] if x else ""  
+    abs_path = ffline[1].split("?", 1)[0] if len(ffline) == 3 else "/"
+    
 print(  )
