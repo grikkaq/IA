2d76ae3d4c7fc9cffd71644dea6ada7416bcf797
grikkaq <melnikov@campus.technion.ac.il>
2019-08-15 20:40:41 +0300

    Update message_info.py

--- message_info.py
+++ message_info.py
@@ -3,38 +3,39 @@
 
 import re
 import sys
-#import io
 
-f = open(sys.argv[1], "rb").read()
-f_splitted = re.split("\n\n", f) # headers @ f_splitted[0] + data @ f_splitted[1]
- 
-# first line either METHOD + ARG + HTTP_Ver, or HTTP_Ver + STATUS + REASON_PHRASE
-ffline = f_splitted[0].split("\n", 1)[0].split(" ", 2)
-ffline = list(filter(None, ffline)) # drop empty strings to ensure abs_path was provided or not
+f = open(sys.argv[1], "r").read()
+# Split file in 2 parts: headers & params @ f_splitted[0] and message body @ f_splitted[1]
+file_splitted = f.split("\n\n", 1) 
 
-if ( re.match( r"HTTP+", ffline[0], re.I ) ):
+# First line either METHOD + ARG + HTTP_Ver, or HTTP_Ver + STATUS + REASON_PHRASE
+first_line = file_splitted[0].split("\n", 1)[0].split(" ", 2)
+# Drop empty strings to ensure abs_path was provided or not
+first_line = list(filter(None, first_line)) 
+
+if re.match(r"HTTP+", first_line[0], re.I):
     message_type = "response"
-    summary = ffline[1] + " " + ffline[2]
+    out = first_line[1] + " " + first_line[2]
 else:
     message_type = "request"
-    x = re.search("Host: [^\\n]+", f_splitted[0] + "\n") # +"\n" in case and Host is the last header 
-    host =  f_splitted[0][ x.start() + len("Host: ") : x.end() ] if x else ""  # port is included
-    abs_path = ffline[1].split("?", 1)[0] if len(ffline) == 3 else "/"
-    url = "http://" + host + abs_path if (ffline[0] != "OPTIONS" or ffline[1] != "*") else "*"
-    summary = ffline[0] + " " + url
+    # Add LF to the end to find Host if it's the last header 
+    x = re.search("Host:[^\\n]+", file_splitted[0] + "\n") 
+    host =  file_splitted[0][x.start() + len("Host: ") : x.end()] if x else ""  # Port is included
+    abs_path = first_line[1].split("?", 1)[0] if len(first_line) == 3 else "/"
+    url = "http://" + host + abs_path if (first_line[0] != "OPTIONS" or first_line[1] != "*") else "*"
+    out = first_line[0] + " " + url
     
-print( summary, end = " " )
-
-body_size = len(f_splitted[1]) if f_splitted[1] else 0
-
-request_headers = { "Accept-Charset", "Accept-Encoding", "Accept-Language", \
-                    "Authorization", "Expect", "From", "Host", "If-Match", \
-                    "If-Modified-Since", "If-None-Match", "If-Range", \
-                    "If-Unmodified-Since", "Max-Forwards", "Proxy-Authorization", \
+request_headers = { "Accept-Charset", "Accept-Encoding", "Accept-Language", 
+                   "Authorization", "Expect", "From", "Host", "If-Match", 
+                   "If-Modified-Since", "If-None-Match", "If-Range", 
+                    "If-Unmodified-Since", "Max-Forwards", "Proxy-Authorization", 
                     "Range", "Referer", "TE", "User-Agent" }
-response_headers = { "Accept-Ranges", "Age", "ETag", "Location", "Proxy-Authenticate", \
+response_headers = { "Accept-Ranges", "Age", "ETag", "Location", "Proxy-Authenticate", 
                     "Retry-After", "Server", "Vary", "WWW-Authenticate" }
-                    
-headers = [line.split(":", 1)[0] for line in f_splitted[0].split("\n")[1:] ] 
-x = len(set(headers).intersection(request_headers if message_type == "request" else response_headers))
-print("(", x, "headers, ", body_size, "bytes)" )
+# Get first word of every line excluding 1st: headers them are
+headers = [ line.split(":")[0] for line in file_splitted[0].split("\n")[1:] ] 
+headers_matched = len(response_headers.intersection(headers)) if message_type == "response" \
+                        else len(request_headers.intersection(headers))
+body_size = len(file_splitted[1]) if len(file_splitted) > 1 else 0  
+
+print(out, " (", headers_matched, " headers, ", body_size , "bytes )")
